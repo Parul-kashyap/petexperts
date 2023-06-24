dc51664d8583ce8364dd74c32d595ed7f7993a46
Parul Kashyap <parulkashyap7140@gmail.com>
2023-06-24 13:43:33 +0530

    bg and book js added

--- signInscript.js
+++ signInscript.js
@@ -1,7 +1,31 @@
 
+
 //===================================== EMAIL ME THE DETAILS OF CUSTOMER =========================
 
 function SendMail(){
+  // FETCHING THE COMPONENTS VIA ID's
+  var name = document.getElementById("fullname");
+  var phoneNo = document.getElementById("phone_no");
+  var trialDate = document.getElementById("trial_date");
+  var address = document.getElementById("address");
+
+  const wait = document.getElementById("wait-msg");
+  const danger = document.getElementById("danger-msg");
+
+  // FIELDS CAN'T BE EMPTY MESSAGE
+  if(name.value === '' || phoneNo.value === '' || trialDate.value === '' || address.value === ''){
+     danger.style.display = "block";
+     }
+     else{
+       danger.style.display = "none";
+        wait.style.display = "block";
+
+        setTimeout(() =>{
+            danger.style.display = "none";
+            wait.style.display = 'none';
+          }, 4000);
+
+// CODE FOR EMAIL VIA EMAIL.JS (please don't make any change)
   var params = {
     from_name : document.getElementById("fullname").value,
     full_name : document.getElementById("fullname").value,
@@ -14,3 +38,4 @@ function SendMail(){
     })
     // .catch((err)=> console.log(err));
   }
+}
