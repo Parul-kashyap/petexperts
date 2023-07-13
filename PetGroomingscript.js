5bbc09a16d6ed7e844b8b1062677f075ba158bd4
Parul Kashyap <parulkashyap7140@gmail.com>
2023-07-13 23:16:19 +0530

    Pet Grooming Added

---
+++ PetGroomingscript.js
@@ -0,0 +1,93 @@
+function goPrevgrooming(){
+  var pggrooming2_21 = document.getElementById("pggrooming2_21");
+  var pggrooming2_22 = document.getElementById("pggrooming2_22");
+  var pggrooming2_23 = document.getElementById("pggrooming2_23");
+  var pggrooming2_24 = document.getElementById("pggrooming2_24");
+  var pggrooming2_25 = document.getElementById("pggrooming2_25");
+
+  // PAGE GROOMING Instructions-------------
+  if(pggrooming2_23.style.display === "block"){
+    pggrooming2_21.style.display = "none";
+    pggrooming2_22.style.display = "block";
+    pggrooming2_23.style.display = "none";
+    pggrooming2_24.style.display = "none";
+    pggrooming2_25.style.display = "none";
+  }
+  else if(pggrooming2_22.style.display === "block"){
+    pggrooming2_21.style.display = "block";
+    pggrooming2_22.style.display = "none";
+    pggrooming2_23.style.display = "none";
+    pggrooming2_24.style.display = "none";
+    pggrooming2_25.style.display = "none";
+  }
+  else if(pggrooming2_24.style.display === "block"){
+    pggrooming2_21.style.display = "none";
+    pggrooming2_22.style.display = "none";
+    pggrooming2_23.style.display = "block";
+    pggrooming2_24.style.display = "none";
+    pggrooming2_25.style.display = "none";
+  }
+  else if(pggrooming2_25.style.display === "block"){
+    pggrooming2_21.style.display = "none";
+    pggrooming2_22.style.display = "none";
+    pggrooming2_23.style.display = "none";
+    pggrooming2_24.style.display = "block";
+    pggrooming2_25.style.display = "none";
+  }
+  else {
+    pggrooming2_21.style.display = "block";
+    pggrooming2_22.style.display = "none";
+    pggrooming2_23.style.display = "none";
+    pggrooming2_24.style.display = "none";
+    pggrooming2_25.style.display = "none";
+    // pg2_2.style.height = "930px";
+  }
+}
+
+function goNextgrooming(){
+  // PAGE 2.2 ---------
+  var pggrooming2_21 = document.getElementById("pggrooming2_21");
+  var pggrooming2_22 = document.getElementById("pggrooming2_22");
+  var pggrooming2_23 = document.getElementById("pggrooming2_23");
+  var pggrooming2_24 = document.getElementById("pggrooming2_24");
+  var pggrooming2_25 = document.getElementById("pggrooming2_25");
+
+  // PAGE GROOMING Instructions-------------
+  if(pggrooming2_22.style.display === "block"){
+    pggrooming2_21.style.display = "none";
+    pggrooming2_22.style.display = "none";
+    pggrooming2_23.style.display = "block";
+    pggrooming2_24.style.display = "none";
+    pggrooming2_25.style.display = "none";
+  }
+  else if(pggrooming2_23.style.display === "block"){
+    pggrooming2_21.style.display = "none";
+    pggrooming2_22.style.display = "none";
+    pggrooming2_23.style.display = "none";
+    pggrooming2_24.style.display = "block";
+    pggrooming2_25.style.display = "none";
+  }
+  else if(pggrooming2_24.style.display === "block"){
+    pggrooming2_21.style.display = "none";
+    pggrooming2_22.style.display = "none";
+    pggrooming2_23.style.display = "none";
+    pggrooming2_24.style.display = "none";
+    pggrooming2_25.style.display = "block";
+  }
+  else if(pggrooming2_25.style.display === "block"){
+    pggrooming2_21.style.display = "none";
+    pggrooming2_22.style.display = "none";
+    pggrooming2_23.style.display = "none";
+    pggrooming2_24.style.display = "none";
+    pggrooming2_25.style.display = "block";
+  }
+  else {
+    pggrooming2_21.style.display = "none";
+    pggrooming2_22.style.display = "block";
+    pggrooming2_23.style.display = "none";
+    pggrooming2_24.style.display = "none";
+    pggrooming2_25.style.display = "none";
+
+  }
+
+}
