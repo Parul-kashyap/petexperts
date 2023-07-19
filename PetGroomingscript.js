d5eb20a5a3ae7f37c5e8b3b17c9ac253f95a278d
Parul Kashyap <parulkashyap7140@gmail.com>
2023-07-19 14:14:30 +0530

    about section part

--- PetGroomingscript.js
+++ PetGroomingscript.js
@@ -8,9 +8,79 @@ function nextProceed(){
 }
 
 
+// BACK BUTTON FOR GROOMING
+
+function backGrooming(){
+  var pg1boxContent =  document.getElementById("boxContent");
+  var pg1boxContent2 = document.getElementById("boxContent2");
+  pg1boxContent.style.display = "block";
+  pg1boxContent2.style.display = "none";
+}
+
+//===================================== EMAIL ME THE DETAILS OF CUSTOMER =========================
+
+// FREE TRAINING SESSION OF DOG
+
+
+function SendMailGrooming(){
+  // FETCHING THE COMPONENTS VIA ID's
+  var name = document.getElementById("nameGrooming");
+  var phoneNo = document.getElementById("numberGrooming");
+  var address = document.getElementById("addressGrooming");
+  var date = document.getElementById("dateGrooming");
+  // TIME SLOTS FOR GROOMING
+  var morning = document.getElementById("morningGrooming");
+  var afternoon = document.getElementById("afternoonGrooming");
+  var evening = document.getElementById("eveningGrooming");
+
+  // WHICH PET
+  var dog = document.getElementById("dog");
+  var cat = document.getElementById("cat");
+
+  // SERVICES
+  var service1 = document.getElementById("service1");
+  var service2 = document.getElementById("service2");
+  var service3 = document.getElementById("service3");
+  var service4 = document.getElementById("service4");
+  var service5 = document.getElementById("service5");
+
+  // MESSAGES AFTER CLICKING BOOK NOW BUTTON
+  var wait = document.getElementById("wait-msg");
+  var danger = document.getElementById("danger-msg");
 
 
 
+
+  // FIELDS CAN'T BE EMPTY MESSAGE
+  if(name.value === '' || phoneNo.value === '' || address.value === '' || date.value === '' || morning.value === '' || afternoon.value ==='' || evening.value === ''){
+     danger.style.display = "block";
+     }
+     else{
+       danger.style.display = "none";
+        wait.style.display = "block";
+
+        setTimeout(() =>{
+            danger.style.display = "none";
+            wait.style.display = 'none';
+          }, 4000);
+
+// CODE FOR EMAIL VIA EMAIL.JS (please don't make any change)
+  var paramGrooming = {
+    from_name : document.getElementById("nameGrooming").value,
+    nameGrooming : document.getElementById("nameGrooming").value,
+    phoneGrooming : document.getElementById("numberGrooming").value,
+    addressGrooming : document.getElementById("addressGrooming").value,
+    dateGrooming : document.getElementById("dateGrooming").value,
+    petGrooming : document.querySelector("input[name='pet']:checked").value,
+    serviceGrooming : document.querySelector("input[name='service']:checked").value,
+    timeSlot : document.querySelector("input[name='timeSlot']:checked").value
+  }
+    emailjs.send("service_ymsxwme","template_avh6k3a", paramGrooming).then(function(res) {
+      alert("Your Pet Grooming Session is Booked..")
+    })
+    // .catch((err)=> console.log(err));
+  }
+}
 
 
 
